5f39b34feac0a17232ff921a0b6c7917645826ed
ZxlZiad <abalil.ziad@gmail.com>
2023-10-28 17:16:28 +0100

    Third Commmit

---
+++ src/main/java/org/sid/bankaccountservice/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package org.sid.bankaccountservice.repositories;
+
+import org.sid.bankaccountservice.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+}
