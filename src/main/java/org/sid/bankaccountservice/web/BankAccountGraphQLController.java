008520eea320efa0bf3f53d673181a14bfe2efa4
ZxlZiad <abalil.ziad@gmail.com>
2023-10-28 14:39:43 +0100

    Second Commmit

---
+++ src/main/java/org/sid/bankaccountservice/web/BankAccountGraphQLController.java
@@ -0,0 +1,20 @@
+package org.sid.bankaccountservice.web;
+
+import org.sid.bankaccountservice.entities.BankAccount;
+import org.sid.bankaccountservice.repositories.BankAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class BankAccountGraphQLController {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+    @QueryMapping
+    public List<BankAccount> accountList(){
+        return bankAccountRepository.findAll();
+    }
+}
